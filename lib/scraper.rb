127fc2f6d49243d4876af9f3345d701c40ea836c
Evan Jeffries <ejeffries14@gmail.com>
2020-04-24 19:45:15 +0000

    update

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,13 @@
-require 'nokogiri'
+require 'Nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
   
-  self.current_drugs
+  def self.current_drugs
   html = open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages")
   doc = Nokogiri::HTML(html)
   binding.pry
-  
   end
+  
 end
