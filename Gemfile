c200914f598b893ffb7ea408482aaa789ae5dc63
Evan Jeffries <ejeffries14@gmail.com>
2020-04-24 15:38:39 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in drug_shortage_cli.gemspec
+gemspec
---
+++ lib/drug_shortage_cli.rb
@@ -0,0 +1,6 @@
+require "drug_shortage_cli/version"
+
+module DrugShortageCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
