caaa4d2c8051e41e0226e82fbc5c7c892d05d4fc
Evan Jeffries <ejeffries14@gmail.com>
2020-05-06 22:08:55 +0000

    element update

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,20 +3,19 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  attr_accessor :name
+  attr_accessor :name, :drug
   
   @@all = {}
-  def initialize
-    @name = name
-    @@all[:name] = @name
-  end
+  #def initialize
+    #@name = name
+   # @@all[:name] = @name
+  #end
   
   def self.current_drugs
   html = open("https://www.ashp.org/Drug-Shortages/Current-Shortages/Drug-Shortages-List?page=CurrentShortages")
   doc = Nokogiri::HTML(html)
-    doc.css(".drug-shortage-container").each do |page|
-      drug = page.css("tbody a").text
-      @name = drug
+    doc.css(".drug-shortage-container").select do |page|
+      name = page.css("tr")
     binding.pry
     end
   end
